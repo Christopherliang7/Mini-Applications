dde982222922315c48f76253e696ee5e33b8691f
Christopher <Christopherliang7@gmail.com>
2020-11-07 22:58:12 -0500

    Created Inputs For All Created Basic CSS Requiring Functions for Updating State & POST request

--- 3-checkout/client/app.jsx
+++ 3-checkout/client/app.jsx
@@ -6,6 +6,7 @@ class App extends React.Component {
       form1: false,
       form2: false,
       form3: false,
+      confirmation: false,
       name: '',
       email: '',
       password: '',
@@ -27,6 +28,7 @@ class App extends React.Component {
     this.openForm1 = this.openForm1.bind(this);
     this.openForm2 = this.openForm2.bind(this);
     this.openForm3 = this.openForm3.bind(this);
+    this.confirmation = this.confirmation.bind(this);
   }
 
   openForm1(event) {
@@ -45,36 +47,94 @@ class App extends React.Component {
     this.setState({form2: false, form3: true})
   }
 
+  confirmation(event) {
+    event.preventDefault()
+    this.setState({form3: false, confirmation: true});
+  }
+
   render() {
     if (this.state.checkout) {
       return (
-        <div>
+        <div className='container'>
           <h1>Illegal Llama Shop</h1>
           <button onClick={this.openForm1}>Checkout</button>
         </div>
       )
     } else if (this.state.form1) {
       return (
-        <div>
+        <div className='container'>
           <h1>Form 1</h1>
+          <form>
+            <input type='text' name='name' placeholder='Name'/>
+            <br />
+            <input type='text' name='email' placeholder='Email'/>
+            <br />
+            <input type='text' name='password' placeholder='Password'/>
+          </form>
           <button onClick={this.openForm2}>Next</button>
         </div>
       )
     } else if (this.state.form2) {
       return (
-        <div>
+        <div className='container'>
           <h1>Form 2</h1>
+          <form>
+            <input type='text' name='line1' placeholder='Address Line 1'/>
+            <br />
+            <input type='text' name='line2' placeholder='Address Line 2'/>
+            <br />
+            <input type='text' name='city' placeholder='City'/>
+            <br />
+            <input type='text' name='state' placeholder='State'/>
+            <br />
+            <input type='text' name='zipcode' placeholder='Zip Code'/>
+          </form>
           <button onClick={this.openForm3}>Next</button>
         </div>
       )
     } else if (this.state.form3) {
       return (
-        <div>
+        <div className='container'>
           <h1>Form 3</h1>
-          {/* <button>Next</button> */}
+          <form>
+            <input type='text' name='phoneNumber' placeholder='Phone Number'/>
+            <br />
+            <input type='text' name='creditCard' placeholder='Credit Card'/>
+            <br />
+            <input type='text' name='expiryDate' placeholder='Expiration Date'/>
+            <br />
+            <input type='text' name='cvv' placeholder='CVV'/>
+            <br />
+            <input type='text' name='billingZip' placeholder='Billing Zip'/>
+          </form>
+          <button onClick={this.confirmation}>Next </button>
+          {/* The function here will send you to the confirmation page */}
         </div>
       )
-    }
+    } else if (this.state.confirmation) {
+      return (
+        <div className='container'>
+          <h1>Review Your Information Before Submission:</h1>
+          <ul>
+            <li>Name: {this.state.name}</li>
+            <li>Email: {this.state.email}</li>
+            <li>Password: {this.state.password}</li>
+            <li>Address Line 1: {this.state.line1}</li>
+            <li>Address Line 2: {this.state.line2}</li>
+            <li>City: {this.state.city}</li>
+            <li>State: {this.state.state}</li>
+            <li>ZipCode:{this.state.zipcode}</li>
+            <li>Phone Number: {this.state.phoneNumber}</li>
+            <li>Credit Card: {this.state.creditCard}</li>
+            <li>Expiration Date: {this.state.expiryDate}</li>
+            <li>CVV: {this.state.cvv}</li>
+            <li>Billing ZipCode: {this.state.billingZip}</li>
+          </ul>
+          {/* The function here will send a post request to store info in database */}
+          {/* The function here will bring you back to home page and set state back to default */}
+        </div>
+      )
+    } 
   }
 }
 
